849599e7e58d934c1f2c84f54f87b2231b68ea26
Tom_Court <tomcourt94@icloud.com>
2023-06-22 13:58:32 +0100

    nav bar works, excluding homepage. Need to fix default home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import About from "./About";
+import Contact from "./Contact";
 import "./App.css";
 
 function App() {
   return (
-<div class="homepage">
-  <h1>Tom's Portfolio</h1>
-  <p>Welcome to my portfolio</p>
-  </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div className="homepage">..</div>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ frontend/src/Mywork.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Mywork() {
+  return (
+    <div className="homepage">
+      <h1>Home</h1>
+      <p>Homepage works.</p>
+    </div>
+  );
+}
+
+export default Mywork;
