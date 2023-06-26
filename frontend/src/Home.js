2b8c6d0677004e86209df638fcffab4139613deb
Tom_Court <tomcourt94@icloud.com>
2023-06-26 17:01:28 +0100

    home page button functional and styles

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,48 +2,60 @@ import React from "react";
 
 function Home() {
   return (
-    <div class="page-content">
+    <div className="page-content">
       <h2>Hello</h2>
       <p>This is the home page.</p>
       <p>
         Below is a list of projects that I have been working on, in order to
-        demonstate my skills as a Junior Software Engineer:
+        demonstrate my skills as a Junior Software Engineer:
       </p>
-      <ul class="home-links">
+      <ul className="home-links">
         <li>
-          <a href="./tictactoe">Tic Tac Toe</a>
+          <h3>Tic Tac Toe</h3>
           <p>
-            A game of tic tac toe usesing simple JavaScript logic, that can be
+            A game of tic tac toe using simple JavaScript logic, that can be
             played by two users on the same computer.
           </p>
-          <button class="tic-tac-toe-go">Go to App</button> <button class="tic-tac-toe-info">More info</button>
+          <a href="./tictactoe">
+            <button className="go-tictactoe">Go to App</button>
+          </a>{" "}
+          <button className="info-tictactoe">More info</button>
         </li>
         <br />
         <li>
-          <a href="./joke">Dad Joke Generator</a>
+          <h3>Dad Joke Generator</h3>
           <p>
             Using a 3rd party API, a random 'Dad Joke' is generated on the
             screen for the user
           </p>
-          <button class="joke-go">Go to App</button> <button class="joke-info">More info</button>
+          <a href="./joke">
+            <button className="go-joke">Go to App</button>
+          </a>{" "}
+          <button className="info-joke">More info</button>
         </li>
         <br />
         <li>
-          <a href="./weather">Weather Forecast</a>
+          <h3>Weather Forecast</h3>
           <p>
             Another application that uses a 3rd party API - this time using real
             time data to fetch weather information for any given U.S. zip code
           </p>
-          <button class="weather-go">Go to App</button> <button class="weather-info">More info</button>
+          <a href="./weather">
+            <button className="go-weather">Go to App</button>
+          </a>{" "}
+          <button className="info-weather">More info</button>
         </li>
         <br />
         <li>
-          <a href="./paint">Paint</a>
+          <h3>Paint</h3>
           <p>
             A JavaScript application that enables users to paint on a blank
             canvas
           </p>
-          <button class="paint-go">Go to App</button> <button class="paint-info">More info</button>
+          <a href="./paint">
+            <button className="go-paint">Go to App</button>
+          </a>{" "}
+          <button className="info-weather">More info</button>
         </li>
         <br />
       </ul>
